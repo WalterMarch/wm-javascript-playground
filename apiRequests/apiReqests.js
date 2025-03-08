9d1580d49d73d03022174cef247dc14e7dd5ad5f
WalterMarch <walter.march@outlook.com>
2025-03-08 11:30:09 +0000

    add post request

--- apiRequests/apiReqests.js
+++ apiRequests/apiReqests.js
@@ -1,11 +1,26 @@
 import axios from 'axios'; 
 
 async function getNewsSections(){
-  const response = await axios.get('https://ok.surf/api/v1/news-section-names')
+  const response = await axios.get("https://ok.surf/api/v1/news-section-names");
   return  response.data; 
+}
+
+async function fetchNews(section){
+  const postBody = {"sections":[section]};
+  const response = await axios.post(
+    "https://ok.surf/api/v1/news-section", 
+    postBody
+  );
+  const output = response.data;
+  return output;
 }
 
 const newsSections = await getNewsSections()
   .then(data => data).catch(err => console.log(err));
 
-console.log(newsSections);
+const randomNewsSection = newsSections[Math.floor(Math.random() * newsSections.length)];
+
+const news = await fetchNews(randomNewsSection)
+  .then(data => data).catch(err => console.log(err));
+
+console.log(news);
